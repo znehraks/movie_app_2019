c09e4def8adf0e3adc8dd12fd9978f5470d7f12f
DesignC <znehraks@gmail.com>
2019-09-29 16:14:57 +0900

    Updates

---
+++ precache-manifest.fd4b7690fafe3d8077685a6a619db498.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "447bfb641096b52cc4e9501ddab004ec",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "6a8f904221f58eea734a",
+    "url": "/movie_app_2019/static/css/main.cd0c9bd6.chunk.css"
+  },
+  {
+    "revision": "84b27ec3c359c1d815f9",
+    "url": "/movie_app_2019/static/js/2.e8b5f4d8.chunk.js"
+  },
+  {
+    "revision": "6a8f904221f58eea734a",
+    "url": "/movie_app_2019/static/js/main.9e312f77.chunk.js"
+  },
+  {
+    "revision": "78bf2593db492f247982",
+    "url": "/movie_app_2019/static/js/runtime-main.b47ea7cd.js"
+  }
+]);
